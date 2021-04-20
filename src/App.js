e3116665cf2c9e09c5bfa7171bc531c2a41169d4
buckethead1986 <rspeckpfs@gmail.com>
2021-04-19 23:56:49 -0700

    reorganizing data files by data type (array, etc)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@
 import "bootstrap/dist/css/bootstrap.min.css"; //redundant because of 'rel="Stylesheet"' link in index.html
 import Appbar from "./components/Appbar.js";
 import TabsContainer from "./components/tabComponents/TabsContainer.js";
+import ArrayTabsContainer from "./components/tabComponents/ArrayTabsContainer.js";
 
 function App() {
   return (
     <div>
       <Appbar />
-      <TabsContainer />
+      <ArrayTabsContainer />
     </div>
   );
 }
--- src/components/tabComponents/ArrayMethodsData.js
+++ src/components/tabComponents/ArrayMethodsData.js
@@ -1,49 +1,21 @@
-export const tabData = [
+export const arrayMethodsData = [
   {
-    eventKey: 1,
-    name: "Functions",
-    description: (
-      <p>
-        Javascript functions are blocks of code designed to perform a particular
-        task.
-      </p>
-    ),
+    eventKey: 0,
+    name: "Array",
+    description:
+      "Arrays are Objects and are used to store multiple values in a single variable. Arrays indexes are zero-based. The first element in an array is 0, the second is 1, etc.",
     sections: [
       {
         id: 0,
-        title: "Function Declaration",
-        description: (
-          <p>Function Declarations start with the keyword 'function'.</p>
-        ),
-        code: `//Basic Syntax:\nfunction functionName(parameter1, parameter2...) = {\n return statement\n}\n\n//Simple Example:\nfunction multiplyByTwo(number) = {\n  return number * 2;\n}\n\n//Expected output:\nconsole.log(multiplyByTwo(4)) //8`
+        title: "Syntax",
+        description: "",
+        code: `const arrayName = [value1, value2, ...]`
       },
       {
         id: 1,
-        title: "Function Expression",
-        description: (
-          <p>Function Expressions start with the keyword 'const'.</p>
-        ),
-        code: `//Basic Syntax:\nconst functionName = function(parameter1, parameter2...) {\n return statement\n}\n\n//Simple Example:\nconst multiplyByThree = function(number) {\n return number * 3;\n}\n\n//Expected Output:\nconsole.log(multiplyByThree(4)) //12`
-      },
-      {
-        id: 2,
-        title: "Arrow Functions",
-        description: (
-          <p>
-            Arrow Functions are written similarly to Function Expressions, with
-            a fat arrow (=>) between the list of parameters and code block.
-            <br />
-            If there&apos;s only one parameter, no parentheses are required.
-            <br />
-            If the code block fits on one line, the curly braces and return
-            statement can be removed.
-            <br />
-            Arrow functions are anonymous (no name), have shorter syntax, and do
-            not have their own 'this', instead using the 'this' context of the
-            enclosing execution context.
-          </p>
-        ),
-        code: `//Basic Syntax:\nconst functionName = (parameter1, parameter2,...) => {\n return statement\n}\n\n//Simple Example:\nconst multiplyByFour = (number) => {\n return number * 4\n}\n\n//Removing the parentheses around a single parameter, and the curly braces and return keyword for a code block requiring only one line:\nconst multiplyByFour = number => number * 4\n\n//Expected Output:\nconsole.log(multiplyByFour(6)) //24`
+        title: "Example",
+        description: "",
+        code: `const simpleArray = ['a', 'b', 'c', 'd', 'e']\nconsole.log(simpleArray[1]) // 'b'\n\nconst variedArray = [13, 'string', {objects: "themselves"}, ["Other", "Arrays"], 900]\nconsole.log(variedArray[0]) // 13\nconsole.log(variedArray[2].objects) // 'themselves'`
       }
     ]
   },
